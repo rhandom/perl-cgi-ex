6d8fb90e455d1381d0db9ed908118b196d2a292d
pauls <pauls>
2003-11-25 22:41:10 +0000

    show the benchmark

--- samples/benchmark/bench_cgix_hfif.pl
+++ samples/benchmark/bench_cgix_hfif.pl
@@ -1,5 +1,16 @@
 #!/usr/bin/perl -w
 
+# [pauls@localhost lib]$ perl ../t/samples/bench_cgix_hfif.pl
+# Benchmark: timing 1000 iterations of cgix_func, cgix_meth, hfif...
+#  cgix_func:  1 wallclock secs ( 1.05 usr +  0.00 sys =  1.05 CPU) @ 952.38/s (n=1000)
+#  cgix_meth:  2 wallclock secs ( 1.09 usr +  0.01 sys =  1.10 CPU) @ 909.09/s (n=1000)
+#  hfif:  8 wallclock secs ( 7.96 usr +  0.02 sys =  7.98 CPU) @ 125.31/s (n=1000)
+#            Rate      hfif cgix_meth cgix_func
+# hfif      125/s        --      -86%      -87%
+# cgix_meth 909/s      625%        --       -5%
+# cgix_func 952/s      660%        5%        --
+# [pauls@localhost lib]$
+
 use strict;
 
 use Benchmark qw(cmpthese);
@@ -75,21 +86,21 @@ my $x = $fo->fill(scalarref => \$t,
 #exit;
 
 cmpthese($n, {
-  one => sub {
+  hfif => sub {
     my $copy = $t;
     my $new = $fif->fill(scalarref => \$copy,
                          fdat => $form,
                          target => 'foo',
                          );
   },
-  two => sub {
+  cgix_meth => sub {
     my $copy = $t;
     $fo->fill(scalarref => \$copy,
               fdat => $form,
               target => 'foo',
               );
   },
-  tyree => sub {
+  cgix_func => sub {
     my $copy = $t;
     &CGI::Ex::Fill::form_fill(\$copy, $form, 'foo');
   },
