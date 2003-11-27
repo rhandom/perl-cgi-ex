0d9e08a802734bef8ef1837e477830c95c073c57
pauls <pauls>
2003-11-27 05:52:11 +0000

    up'ed the version

--- lib/CGI/Ex.pm
+++ lib/CGI/Ex.pm
@@ -23,7 +23,7 @@ use vars qw($VERSION
 use base qw(Exporter);
 use Data::DumpEx;
 
-$VERSION               = '0.93';
+$VERSION               = '0.94';
 $PREFERRED_FILL_MODULE ||= '';
 $PREFERRED_CGI_MODULE  ||= 'CGI';
 $PREFERRED_VAL_MODULE  ||= '';
