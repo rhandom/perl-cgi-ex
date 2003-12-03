acc2cd54afb35450f644267869ead00c709de198
pauls <pauls>
2003-12-03 19:11:07 +0000

    version number

--- lib/CGI/Ex/Fill.pm
+++ lib/CGI/Ex/Fill.pm
@@ -21,7 +21,7 @@ use vars qw($VERSION
             );
 use Exporter;
 
-$VERSION   = '1.0';
+$VERSION   = '1.1';
 @ISA       = qw(Exporter);
 @EXPORT    = qw(form_fill);
 @EXPORT_OK = qw(form_fill html_escape get_tagval_by_key swap_tagval_by_key);
