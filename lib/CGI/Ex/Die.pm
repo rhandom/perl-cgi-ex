44bc17ddd1ed087e8969fdca6e3ff3c8529e4ac4
pauls <pauls>
2004-11-05 19:14:06 +0000

    allow for more information

--- lib/CGI/Ex/Die.pm
+++ lib/CGI/Ex/Die.pm
@@ -1,12 +1,14 @@
 package CGI::Ex::Die;
 
 use strict;
-use vars qw($no_recurse $EXTENDED_ERRORS);
+use vars qw($no_recurse $EXTENDED_ERRORS $SHOW_TRACE $IGNORE_EVAL);
 
 use CGI::Ex;
-use CGI::Ex::Dump qw(debug);
+use CGI::Ex::Dump qw(debug ctrace);
 
 BEGIN {
+  $SHOW_TRACE = 0      if ! defined $SHOW_TRACE;
+  $IGNORE_EVAL = 0     if ! defined $IGNORE_EVAL;
   $EXTENDED_ERRORS = 1 if ! defined $EXTENDED_ERRORS;
 }
 
@@ -27,6 +29,9 @@ sub import {
     if (! ref($args{register}) || grep {/die/} @{ $args{register} }) {
       $SIG{__DIE__} = \&die_handler;
     }
+    $SHOW_TRACE      = $args{'show_trace'}      if exists $args{'show_trace'};
+    $IGNORE_EVAL     = $args{'ignore_eval'}     if exists $args{'ignore_eval'};
+    $EXTENDED_ERRORS = $args{'extended_errors'} if exists $args{'extended_errors'};
   }
   return 1;
 }
@@ -40,23 +45,26 @@ sub die_handler {
   local $no_recurse = 1;
 
   ### test for eval - if eval - propogate it up
-  if (! $ENV{MOD_PERL}) {
-    my $n = 0;
-    while (my $sub = (caller(++$n))[3]) {
-      next if $sub !~ /eval/;
-      die $err; # die and let the eval catch it
-    }
-  }
+  if (! $IGNORE_EVAL) {
+    if (! $ENV{MOD_PERL}) {
+      my $n = 0;
+      while (my $sub = (caller(++$n))[3]) {
+        next if $sub !~ /eval/;
+        die $err; # die and let the eval catch it
+      }
 
-  ### test for eval in a mod_perl environment
-  my $n     = 0;
-  my $found = 0;
-  while (my $sub = (caller(++$n))[3]) {
-    $found = $n if ! $found && $sub =~ /eval/;
-    last if $sub =~ /^Apache::(PerlRun|Registry)/;
-  }
-  if ($found && $n - 1 != $found) {
-    die $err;
+      ### test for eval in a mod_perl environment
+    } else {
+      my $n     = 0;
+      my $found = 0;
+      while (my $sub = (caller(++$n))[3]) {
+        $found = $n if ! $found && $sub =~ /eval/;
+        last if $sub =~ /^(Apache|ModPerl)::(PerlRun|Registry)/;
+      }
+      if ($found && $n - 1 != $found) {
+        die $err;
+      }
+    }
   }
 
   ### decode the message
@@ -79,22 +87,27 @@ sub die_handler {
   my $msg = &CGI::Ex::Dump::_html_quote("$err");
   $msg = "<pre style='background:red;color:white;border:2px solid black;font-size:120%;padding:3px'>Error: $msg</pre>\n";
 
-  ### similar to CGI::Carp
-  if ($ENV{MOD_PERL} && (my $r = Apache->request)) {
-    if ($r->bytes_sent) {
-      $r->print($msg);
-      $r->exit;
+  if ($ENV{REQUEST_METHOD}) {
+    ### similar to CGI::Carp
+    if (my $r = CGI::Ex->apache_request) {
+      if ($r->bytes_sent) {
+        $r->print($msg);
+        $r->exit;
+      } else {
+        $r->status(500);
+        $r->custom_response(500,$msg);
+      }
     } else {
-      $r->status(500);
-      $r->custom_response(500,$msg);
+      CGI::Ex->print_content_type;
+      print $msg;
     }
-  } elsif ($ENV{REQUEST_METHOD}) {
-    &CGI::Ex::content_type();
-    print $msg;
+    ### show the trace
+    debug ctrace
+      if $SHOW_TRACE;
   } else {
     ### command line execution
   }
-  
+
   die $err;
 }
 
