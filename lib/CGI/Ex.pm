2ad1b3ebce7da75d10a53013321e958dc2c50e15
pauls <pauls>
2004-11-10 20:47:17 +0000

    uped version

--- lib/CGI/Ex.pm
+++ lib/CGI/Ex.pm
@@ -24,7 +24,7 @@ use vars qw($VERSION
             );
 use base qw(Exporter);
 
-$VERSION               = '1.11';
+$VERSION               = '1.12';
 $PREFERRED_FILL_MODULE ||= '';
 $PREFERRED_CGI_MODULE  ||= 'CGI';
 $PREFERRED_VAL_MODULE  ||= '';
