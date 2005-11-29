151b7113e0fa29d4ba7c9537ddcc5ee9f3698b17
pauls <pauls>
2005-11-29 04:43:25 +0000

    add some more samples

--- samples/devel/dprof_conf.d
+++ samples/devel/dprof_conf.d
@@ -28,13 +28,16 @@ conf_write($tmp, $conf);
 
 for (1 .. 100_000) {
 #    my $ref = $cob->read($tmp);
-    my $ref = conf_read($tmp);
+#    my $ref = conf_read($tmp);
 #    print Dumper $ref; exit;
+
+    conf_write($tmp, $conf);
 }
 
 
 __END__
 
+### conf_read
 %Time ExclSec CumulS #Calls sec/call Csec/c  Name
  38.5   2.120  0.000 100000   0.0000 0.0000  Storable::_retrieve
  38.1   2.100  2.100 100000   0.0000 0.0000  Storable::pretrieve
@@ -53,3 +56,22 @@ __END__
  0.00   0.000  0.000      1   0.0000 0.0000  POSIX::load_imports
  0.00   0.000  0.000      1   0.0000 0.0000  Exporter::Heavy::heavy_export
  0.00   0.000  0.000      1   0.0000 0.0000  Storable::store
+
+### conf_write
+%Time ExclSec CumulS #Calls sec/call Csec/c  Name
+ 60.3   9.510  9.510 100001   0.0001 0.0001  Storable::pstore
+ 32.8   5.170  0.000 100001   0.0001 0.0000  Storable::_store
+ 7.68   1.210 16.450 100001   0.0000 0.0002  CGI::Ex::Conf::write_ref
+ 2.60   0.410 17.220 100001   0.0000 0.0002  CGI::Ex::Conf::conf_write
+ 2.28   0.360  0.360 100001   0.0000 0.0000  CGI::Ex::Conf::new
+ 2.16   0.340 15.240 100001   0.0000 0.0002  CGI::Ex::Conf::write_handler_stora
+                                             ble
+ 1.33   0.210  0.000 100001   0.0000 0.0000  Storable::store
+ 0.06   0.010  0.010      3   0.0033 0.0033  AutoLoader::import
+ 0.06   0.010  0.010      2   0.0050 0.0050  DynaLoader::BEGIN
+ 0.06   0.010  0.010      4   0.0025 0.0025  CGI::Ex::Conf::BEGIN
+ 0.06   0.010  0.030      3   0.0033 0.0099  main::BEGIN
+ 0.00   0.000  0.000      1   0.0000 0.0000  POSIX::load_imports
+ 0.00   0.000  0.000      1   0.0000 0.0000  Exporter::Heavy::heavy_export
+ 0.00       - -0.000      1        -      -  main::END
+ 0.00       - -0.000      1        -      -  bytes::import
