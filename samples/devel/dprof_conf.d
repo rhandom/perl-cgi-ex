b4b21ef6f1b4172fcac31f53f0bdbe625fca866d
pauls <pauls>
2005-11-29 04:37:43 +0000

    update

--- samples/devel/dprof_conf.d
+++ samples/devel/dprof_conf.d
@@ -32,3 +32,24 @@ for (1 .. 100_000) {
 #    print Dumper $ref; exit;
 }
 
+
+__END__
+
+%Time ExclSec CumulS #Calls sec/call Csec/c  Name
+ 38.5   2.120  0.000 100000   0.0000 0.0000  Storable::_retrieve
+ 38.1   2.100  2.100 100000   0.0000 0.0000  Storable::pretrieve
+ 20.9   1.150  5.860 100000   0.0000 0.0001  CGI::Ex::Conf::read_ref
+ 8.73   0.480  6.720 100000   0.0000 0.0001  CGI::Ex::Conf::conf_read
+ 6.91   0.380  0.380 100001   0.0000 0.0000  CGI::Ex::Conf::new
+ 4.73   0.260  0.000 100000   0.0000 0.0000  Storable::retrieve
+ 4.18   0.230  4.710 100000   0.0000 0.0000  CGI::Ex::Conf::read_handler_storab
+                                             le
+ 0.36   0.020  0.040      3   0.0067 0.0133  main::BEGIN
+ 0.18   0.010  0.010      6   0.0017 0.0017  Exporter::import
+ 0.18   0.010  0.010      4   0.0025 0.0025  CGI::Ex::Conf::BEGIN
+ 0.18   0.010  0.020      1   0.0100 0.0199  CGI::Ex::Conf::write_handler_stora
+                                             ble
+ 0.18   0.010  0.010      5   0.0020 0.0020  AutoLoader::AUTOLOAD
+ 0.00   0.000  0.000      1   0.0000 0.0000  POSIX::load_imports
+ 0.00   0.000  0.000      1   0.0000 0.0000  Exporter::Heavy::heavy_export
+ 0.00   0.000  0.000      1   0.0000 0.0000  Storable::store
