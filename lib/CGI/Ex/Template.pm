4d1e185daa388737ad98e4a5cd2efa57f746a317
pauls <pauls>
2005-12-06 18:56:28 +0000

    begin to move templating into here

--- lib/CGI/Ex/Template.pm
+++ lib/CGI/Ex/Template.pm
@@ -1,13 +1,17 @@
 package CGI::Ex::Template;
 
 use strict;
-use vars qw(@INCLUDE_PATH $CONTENT_SUBDIR);
-use base qw(Template);
+use vars qw(@INCLUDE_PATH
+            $CONTENT_SUBDIR
+            $TEMPLATE_OPEN
+            $TEMPLATE_CLOSE
+            );
 
-use CGI::Ex;
-use CGI::Ex::Fill;
-
-$CONTENT_SUBDIR ||= 'content';
+BEGIN {
+    $CONTENT_SUBDIR ||= 'content';
+    $TEMPLATE_OPEN  ||= qr/\[%\s*/;
+    $TEMPLATE_CLOSE ||= qr/\s*%\]/;
+};
 
 ###----------------------------------------------------------------###
 
@@ -17,8 +21,85 @@ sub new {
 
   $args->{INCLUDE_PATH} ||= \@INCLUDE_PATH;
 
-  return $class->SUPER::new($args);
+  return bless $args, $class;
 }
+
+###----------------------------------------------------------------###
+
+sub stash {
+    my $self = shift;
+    if (@_) {
+        die "Stash must be a hashref" if ! UNIVERSAL::isa($_[0], 'HASH');
+        $self->{'STASH'} = shift;
+    }
+    return $self->{'STASH'} ||= {};
+}
+
+### This is intended as a simple yet strong subroutine to swap
+### in tags to a document.  It is intended to be very basic
+### for those who may not want the full features of a Templating
+### system such as Template::Toolkit (even though they should
+### investigate them because they are pretty nice)
+sub swap {
+  my $self = shift;
+  my $str  = shift;
+  my $form = shift;
+  $self->stash($form) if $form;
+
+  return $str if ! $str;
+  my $ref  = ref($str) ? $str : \$str;
+
+  $self->swap_variables($ref);
+
+  return ref($str) ? 1 : $$ref;
+}
+
+sub swap_variables {
+    my $self = shift;
+    my $ref  = shift;
+    my $stash = $self->stash;
+
+  ### now do the swap
+  $$ref =~ s{$TEMPLATE_OPEN \b (\w+) ((?:\.\w+)*) \b $TEMPLATE_CLOSE}{
+    my ($name, $extra) = ($1, $2);
+    my $val;
+    if (! $extra) {
+      $val = defined($stash->{$name}) ? $stash->{$name} : '';
+    } else {
+      my @extra = split(/\./, substr($extra,1));
+      my $ref   = defined($stash->{$name}) ? $stash->{$name} : '';
+      while (defined(my $key = shift(@extra))) {
+        if (UNIVERSAL::isa($ref, 'HASH')) {
+          if (! exists($ref->{$key}) || ! defined($ref->{$key})) {
+            $val = '';
+            last;
+          }
+          $ref = $ref->{$key};
+        } elsif (UNIVERSAL::isa($ref, 'ARRAY')) {
+          if (! exists($ref->[$key]) || ! defined($ref->[$key])) {
+            $val = '';
+            last;
+          }
+          $ref = $ref->[$key];
+        } else {
+          $val = '';
+          last;
+        }
+      }
+      if (! defined($val)) {
+        if ($#extra == -1) {
+          $val = $ref;
+        }
+        $val = '' if ! defined($val);
+      }
+    }
+    $val; # return of the swap
+  }xeg;
+
+}
+
+###----------------------------------------------------------------###
+
 
 sub process {
   my $self = ref($_[0]) ? shift : shift->new;
@@ -53,19 +134,8 @@ sub out {
   my $status = $self->process($in, $form, \$out) || die $Template::ERROR;
 
   ### fill in any forms
-  &CGI::Ex::Fill::form_fill(\$out, $fill) if $fill && ! $self->{no_fill};
 
   return $out;
-}
-
-sub print {
-  my $self   = ref($_[0]) ? shift : shift->new;
-  my $in     = shift;
-  my $form   = shift;
-  my $fill   = shift || $form;
-
-  &CGI::Ex::content_type();
-  print $self->out($in, $form, $fill);
 }
 
 ###----------------------------------------------------------------###
