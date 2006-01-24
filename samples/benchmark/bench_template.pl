09b7793a4d44da4c838bd7e25939b221da55f8a9
pauls <pauls>
2006-01-24 05:21:32 +0000

    show more benchmarks

--- samples/benchmark/bench_template.pl
+++ samples/benchmark/bench_template.pl
@@ -1,11 +1,16 @@
 #!/usr/bin/perl -w
 
+=head1 NAME
+
+bench_template.pl - Test relative performance of CGI::Ex::Template to Template::Toolkit
+
+=cut
+
 use strict;
 use Benchmark qw(cmpthese timethese);
 use POSIX qw(tmpnam);
 use File::Path qw(rmtree);
 use CGI::Ex::Template;
-#use CGI::Ex::Template2;
 use CGI::Ex::Dump qw(debug);
 use Template;
 
@@ -18,7 +23,6 @@ my $tt2 = Template->new(ABSOLUTE => 1, COMPILE_DIR => $tt_cache_dir, COMPILE_EXT
 
 my $cet = CGI::Ex::Template->new(ABSOLUTE => 1);
 my $cetc = CGI::Ex::Template->new(ABSOLUTE => 1, COMPILE_DIR => $tt_cache_dir, COMPILE_EXT => 'ttc');
-#my $cet2 = CGI::Ex::Template2->new(ABSOLUTE => 1);
 
 
 ###----------------------------------------------------------------###
@@ -33,43 +37,52 @@ my $swap = {
     code  => sub {"($_[0])"},
     cet   => $cet,
 };
-#$swap->{$_} = $_ for (1 .. 1000);
+#$swap->{$_} = $_ for (1 .. 1000); # swap size affects benchmark speed
 
-my $txt;
-                                                            # This percent is compiled (in memory) CET vs TT
-#$txt = ((" "x1000)."[% one %]\n")x10;                      #   69%
-#$txt = ((" "x1000)."[% one %]\n")x100;                     #   42%
-$txt = ((" "x10)."[% one %]\n")x1000;                      #   -6%
-#$txt = ("[% \"".(" "x10)."\$one\" %]\n")x1000;             #  -60%
-#$txt = "[% one %]";                                        #  253%
-#$txt = "[% one %]"x20;                                     #   44%
-#$txt = "([% 1 + 2 %])";                                    #   49%
-#$txt = "[% 1 + 2 + 3 + 5 + 6 + 8 %]";                      #  118%
-#$txt = "[% SET one = 2 %]";                                #  196%
-#$txt = "[% SET one = [0..30] %]";                          #   42%
-#$txt = "[% c.d.0.hee.0 %]";                                #  280%
-#$txt = ((" "x10)."[% c.d.0.hee.0 %]\n")x1000;              #   62%
-#$txt = "[% t = 1 || 0 ? 0 : 1 || 2 ? 2 : 3 %][% t %]";     #  109%
-#$txt = "[% IF 1 %]Two[% END %]";                           #  180%
-#$txt = "[% FOREACH i = [0..10] %][% i %][% END %]";        #    8%
-#$txt = "[%i=1 ; FOREACH [0..10] ; i ; END ; i %]";         #   36%
-#$txt = "[%i=1 ; FOREACH i = [0..100] ; i ; END ; i%]";     #  -12%
-#$txt = "[%i=1 ; FOREACH [0..100] ; i ; END ; i %]";        #   11%
-#$txt = "[%f=10%][%WHILE f%][%f=f- 1%][%f%][% END %]";      #  -16%
-#$txt = "[%f=10; WHILE f ; f = f - 1 ; f ; END %]";         #  -15%
-#$txt = "[%f=10; WHILE (g=f) ; f = f - 1 ; f ; END %]";     #   -7%
-#$txt = "[%f=1; WHILE (g=f) ; f = f - 1 ; f ; END %]";      #   75%
-#$txt = "[%f=5; WHILE (g=f) ; f = f - 1 ; f ; END %]";      #    9%
-#$txt = "[% BLOCK foo %]Hi[% END %][% PROCESS foo %]";      #  382%
-#$txt = "[% BLOCK foo %]Hi[% END %][% INCLUDE foo %]";      #  330%
-#$txt = "[% BLOCK foo %]Hi[% END %][% INCLUDE foo %]";      #  330%
-#$txt = "[% MACRO foo BLOCK %]Hi[% END %][% foo %]";        #  140%
-#$txt = "[% MACRO foo(n) BLOCK %]Hi[%n%][%END%][%foo(2)%]"; #  105%
-#$txt = "[% MACRO foo PROCESS bar;BLOCK bar%]7[%END;foo%]"; #  174%
+### uncomment any of the lines below to run the test
+                                                            ### All percents are CGI::Ex::Template vs TT2
+                                                            ### (The percent that CET is faster than TT)
+                                                            # This percent is compiled in memory (repeated calls)
+                                                            #           # New object each time (undef CACHE_SIZE)
+                                                            #           #           # New object with CACHE_DIR set
+my $txt = "";                                               #  540%     # 1067%     #  608%
+#$txt = ((" "x1000)."[% one %]\n")x10;                      #   79%     #  637%     #  265%
+#$txt = ((" "x1000)."[% one %]\n")x100;                     #   40%     #  560%     #  127%
+$txt = ((" "x10)."[% one %]\n")x1000;                      #   -6%     #  415%     #   24%
+#$txt = ("[% \"".(" "x1000)."\$one\" %]\n")x10;             #  -32%     #  313270%  #  102%
+#$txt = ("[% \"".(" "x10)."\$one\" %]\n")x1000;             #  -59%     #  335%     #  -30%
+#$txt = "[% one %]";                                        #  320%     #  902%     #  583%
+#$txt = "[% one %]"x20;                                     #   48%     #  470%     #  161%
+#$txt = "[% one %]"x200;                                    #    8%     #  381%     #   36%
+#$txt = "([% 1 + 2 %])";                                    #  134%     #  635%     #  382%
+#$txt = "[% 1 + 2 + 3 + 5 + 6 + 8 %]";                      #  110%     #  426%     #  384%
+#$txt = "[% SET one = 2 %]";                                #  296%     #  778%     #  528%
+#$txt = "[% SET one = [0..30] %]";                          #   63%     #  441%     #  304%
+#$txt = "[% c.d.0.hee.0 %]";                                #  356%     #  973%     #  572%
+#$txt = ((" "x10)."[% c.d.0.hee.0 %]\n")x1000;              #   66%     #  626%     #   61%
+#$txt = "[% t = 1 || 0 ? 0 : 1 || 2 ? 2 : 3 %][% t %]";     #  108%     #  381%     #  316%
+#$txt = "[% a=1 %][% IF a %]Two[% END %]";                  #  193%     #  650%     #  419%
+#$txt = "[% FOREACH i = [0..10] %][% i %][% END %]";        #   18%     #  263%     #  160%
+#$txt = "[%i=1 ; FOREACH i = [0..100] ; i ; END ; i%]";     #  -17%     #   46%     #   12%
+#$txt = "[%i=1 ; FOREACH i = [0..1000] ; i ; END ; i%]";    #  -19%     #  -12%     #  -17%
+#$txt = "[%i=1 ; FOREACH [0..10] ; i ; END ; i %]";         #   34%     #  305%     #  181%
+#$txt = "[%i=1 ; FOREACH [0..100] ; i ; END ; i %]";        #    7%     #   82%     #   42%
+#$txt = "[%i=1 ; FOREACH [0..1000] ; i ; END ; i %]";       #    0%     #   12%     #    6%
+#$txt = "[%f=10%][%WHILE f%][%f=f- 1%][%f%][% END %]";      #  -17%     #  153%     #   48%
+#$txt = "[%f=10; WHILE (g=f) ; f = f - 1 ; f ; END %]";     #   -9%     #  148%     #   53%
+#$txt = "[%f=5; WHILE (g=f) ; f = f - 1 ; f ; END %]";      #    6%     #  244%     #  102%
+#$txt = "[%f=1; WHILE (g=f) ; f = f - 1 ; f ; END %]";      #   74%     #  422%     #  252%
+#$txt = "[% BLOCK foo %]Hi[% END %][% PROCESS foo %]";      #  358%     #  843%     #  572%
+#$txt = "[% BLOCK foo %]Hi[% END %][% INCLUDE foo %]";      #  314%     #  798%     #  545%
+#$txt = "[% MACRO foo BLOCK %]Hi[% END %][% foo %]";        #  135%     #  512%     #  356%
+#$txt = "[% MACRO foo(n) BLOCK %]Hi[%n%][%END%][%foo(2)%]"; #  102%     #  345%     #  308%
+#$txt = "[% MACRO foo PROCESS bar;BLOCK bar%]7[%END;foo%]"; #  174%     #  449%     #  415%
+#$txt = "[% n = 1 %][% n FILTER repeat(2) %]";              #  114%     #  453%     #  343%
+#$txt = "[% n=1; n FILTER echo=repeat(2); n FILTER echo%]"; #   40%     #  375%     #  243%
 
-my $file  = \$txt;
-my $file2 = $tt_cache_dir .'/template.txt';
-open(my $fh, ">$file2") || die "Couldn't open $file2: $!";
+my $str_ref = \$txt;
+my $filename = $tt_cache_dir .'/template.txt';
+open(my $fh, ">$filename") || die "Couldn't open $filename: $!";
 print $fh $txt;
 close $fh;
 
@@ -78,33 +91,33 @@ close $fh;
 sub file_TT_new {
     my $out = '';
     my $t = Template->new(ABSOLUTE => 1);
-    $t->process($file2, $swap, \$out);
+    $t->process($filename, $swap, \$out);
     return $out;
 }
 
 sub str_TT_new {
     my $out = '';
     my $t = Template->new(ABSOLUTE => 1);
-    $t->process($file, $swap, \$out);
+    $t->process($str_ref, $swap, \$out);
     return $out;
 }
 
 sub file_TT {
     my $out = '';
-    $tt1->process($file2, $swap, \$out);
+    $tt1->process($filename, $swap, \$out);
     return $out;
 }
 
 sub str_TT {
     my $out = '';
-    $tt1->process($file, $swap, \$out) || debug $tt1->error;
+    $tt1->process($str_ref, $swap, \$out) || debug $tt1->error;
     return $out;
 }
 
 sub file_TT_cache_new {
     my $out = '';
     my $t = Template->new(ABSOLUTE => 1, COMPILE_DIR => $tt_cache_dir, COMPILE_EXT => 'ttc');
-    $t->process($file2, $swap, \$out);
+    $t->process($filename, $swap, \$out);
     return $out;
 }
 
@@ -113,46 +126,40 @@ sub file_TT_cache_new {
 sub file_CET_new {
     my $out = '';
     my $t = CGI::Ex::Template->new(ABSOLUTE => 1);
-    $t->process($file2, $swap, \$out);
+    $t->process($filename, $swap, \$out);
     return $out;
 }
 
 sub str_CET_new {
     my $out = '';
     my $t = CGI::Ex::Template->new(ABSOLUTE => 1);
-    $t->process($file, $swap, \$out);
+    $t->process($str_ref, $swap, \$out);
     return $out;
 }
 
 sub file_CET {
     my $out = '';
-    $cet->process($file2, $swap, \$out);
+    $cet->process($filename, $swap, \$out);
     return $out;
 }
 
 sub str_CET {
     my $out = '';
-    $cet->process($file, $swap, \$out);
+    $cet->process($str_ref, $swap, \$out);
     return $out;
 }
 
 sub str_CET_swap {
-    my $txt = $cet->swap($file, $swap);
+    my $txt = $cet->swap($str_ref, $swap);
     return $txt;
 }
 
 sub file_CET_cache_new {
     my $out = '';
     my $t = CGI::Ex::Template->new(ABSOLUTE => 1, COMPILE_DIR => $tt_cache_dir, COMPILE_EXT => 'ttc');
-    $t->process($file2, $swap, \$out);
+    $t->process($filename, $swap, \$out);
     return $out;
 }
-
-#sub str_CET_old {
-#    my $out = '';
-#    $cet2->process($file, $swap, \$out);
-#    return $out;
-#}
 
 #debug file_CET(), str_TT();
 #debug $cet->parse_tree($file);
@@ -160,7 +167,7 @@ sub file_CET_cache_new {
 ### check out put - and also allow for caching
 for (1..2) {
     if (file_CET() ne str_TT()) {
-        debug $cet->parse_tree($file);
+        debug $cet->parse_tree($str_ref);
         debug file_CET(), str_TT();
         die "file_CET didn't match";
     }
@@ -170,23 +177,21 @@ for (1..2) {
     die "file_CET_cache_new didn't match " if file_CET_cache_new() ne str_TT();
     die "file_TT_cache_new didn't match " if file_TT_cache_new() ne str_TT();
 }
-
 ###----------------------------------------------------------------###
 
 cmpthese timethese (-2, {
     file_TT_n   => \&file_TT_new,
-    str_TT_n    => \&str_TT_new,
+#    str_TT_n    => \&str_TT_new,
     file_TT     => \&file_TT,
     str_TT      => \&str_TT,
     file_TT_c_n => \&file_TT_cache_new,
 
     file_CT_n   => \&file_CET_new,
-    str_CT_n    => \&str_CET_new,
+#    str_CT_n    => \&str_CET_new,
     file_CT     => \&file_CET,
     str_CT      => \&str_CET,
     str_CT_sw   => \&str_CET_swap,
     file_CT_c_n => \&file_CET_cache_new,
-#    str_CT_o    => \&str_CET_old,
 });
 
 
