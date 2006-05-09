928822cb5dc38121f2091958f9dd14660fafef7b
pauls <pauls>
2006-05-09 22:03:28 +0000

    update

--- samples/benchmark/bench_auth.pl
+++ samples/benchmark/bench_auth.pl
@@ -7,21 +7,21 @@ bench_auth.pl - Test relative performance of CGI::Ex::Auth
 =head1 SAMPLE OUTPUT
 
   Benchmark: running cookie_bad, cookie_good, cookie_good2, form_bad, form_good, form_good2, form_good3 for at least 2 CPU seconds...
-  cookie_bad:  3 wallclock secs ( 2.05 usr +  0.00 sys =  2.05 CPU) @ 6390.73/s (n=13101)
-  cookie_good:  3 wallclock secs ( 2.12 usr +  0.06 sys =  2.18 CPU) @ 6573.39/s (n=14330)
-  cookie_good2:  2 wallclock secs ( 2.05 usr +  0.11 sys =  2.16 CPU) @ 5851.39/s (n=12639)
-    form_bad:  2 wallclock secs ( 2.11 usr +  0.00 sys =  2.11 CPU) @ 6791.00/s (n=14329)
-   form_good:  2 wallclock secs ( 2.03 usr +  0.06 sys =  2.09 CPU) @ 6397.13/s (n=13370)
-  form_good2:  2 wallclock secs ( 1.96 usr +  0.12 sys =  2.08 CPU) @ 5800.48/s (n=12065)
-  form_good3:  3 wallclock secs ( 2.11 usr +  0.00 sys =  2.11 CPU) @ 7838.39/s (n=16539)
-                 Rate form_good2 cookie_good2 cookie_bad form_good cookie_good form_bad form_good3
-  form_good2   5800/s         --          -1%        -9%       -9%        -12%     -15%       -26%
-  cookie_good2 5851/s         1%           --        -8%       -9%        -11%     -14%       -25%
-  cookie_bad   6391/s        10%           9%         --       -0%         -3%      -6%       -18%
-  form_good    6397/s        10%           9%         0%        --         -3%      -6%       -18%
-  cookie_good  6573/s        13%          12%         3%        3%          --      -3%       -16%
-  form_bad     6791/s        17%          16%         6%        6%          3%       --       -13%
-  form_good3   7838/s        35%          34%        23%       23%         19%      15%         --
+  cookie_bad:  2 wallclock secs ( 2.01 usr +  0.00 sys =  2.01 CPU) @ 6982.59/s (n=14035)
+  cookie_good:  3 wallclock secs ( 2.15 usr +  0.06 sys =  2.21 CPU) @ 6484.16/s (n=14330)
+  cookie_good2:  3 wallclock secs ( 2.02 usr +  0.09 sys =  2.11 CPU) @ 5422.27/s (n=11441)
+    form_bad:  3 wallclock secs ( 2.16 usr +  0.01 sys =  2.17 CPU) @ 6603.23/s (n=14329)
+   form_good:  3 wallclock secs ( 2.07 usr +  0.06 sys =  2.13 CPU) @ 6304.69/s (n=13429)
+  form_good2:  3 wallclock secs ( 2.00 usr +  0.12 sys =  2.12 CPU) @ 5396.70/s (n=11441)
+  form_good3:  2 wallclock secs ( 2.14 usr +  0.00 sys =  2.14 CPU) @ 7728.50/s (n=16539)
+                 Rate form_good2 cookie_good2 form_good cookie_good form_bad cookie_bad form_good3
+  form_good2   5397/s         --          -0%      -14%        -17%     -18%       -23%       -30%
+  cookie_good2 5422/s         0%           --      -14%        -16%     -18%       -22%       -30%
+  form_good    6305/s        17%          16%        --         -3%      -5%       -10%       -18%
+  cookie_good  6484/s        20%          20%        3%          --      -2%        -7%       -16%
+  form_bad     6603/s        22%          22%        5%          2%       --        -5%       -15%
+  cookie_bad   6983/s        29%          29%       11%          8%       6%         --       -10%
+  form_good3   7729/s        43%          43%       23%         19%      17%        11%         --
 
 =cut
 
@@ -42,6 +42,7 @@ use CGI::Ex::Dump qw(debug);
     sub get_pass_by_user { '123qwe' }
     sub script_name      { $0 }
     sub no_cookie_verify { 1 }
+    sub secure_hash_keys { ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'bbbbbbbbbbbbbbbbbbbbbbbbbbb', 'ccc'] }
 }
 
 {
@@ -78,6 +79,7 @@ die "set_cookie not called"        if ! $Auth::set_cookie;
 die "delete_cookie was called"     if $Auth::deleted_cookie;
 
 $Auth::printed = $Auth::set_cookie = $Auth::delete_cookie = 0;
+debug form_good2(), (my $e = $@);
 die "Didn't get good auth"         if ! form_good2();
 die "printed was set"              if $Auth::printed;
 die "set_cookie not called"        if ! $Auth::set_cookie;
