612fee5ee86b2ebec9347608aa318192840e406e
pauls <pauls>
2006-05-15 21:26:21 +0000

    first

---
+++ lib/CGI/Ex/Exp.pm
@@ -0,0 +1,21 @@
+package CGI::Ex::Exp;
+
+=head1 NAME
+
+CGI::Ex::Exp - Expression parsing and execution for CGI::Ex::Template (and other takers)
+
+=cut
+
+use strict;
+
+
+
+
+
+
+
+
+
+###----------------------------------------------------------------###
+
+1;
