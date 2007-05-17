52d4c584d6c6308d258b80fda5e581e82a837854
paul <paul>
2007-05-17 22:34:48 +0000

    working TMPL_IF

--- lib/CGI/Ex/Template/Extra.pm
+++ lib/CGI/Ex/Template/Extra.pm
@@ -482,6 +482,271 @@ sub list_plugins {
 
 ###----------------------------------------------------------------###
 
+sub parse_tree_hte {
+    my $self    = shift;
+    my $str_ref = shift;
+    if (! $str_ref || ! defined $$str_ref) {
+        $self->throw('parse.no_string', "No string or undefined during parse");
+    }
+
+    my $START = qr{<(|!--\s*)(/?)[Tt][Mm][Pp][Ll]_(\w+)\b\s*};
+    local $self->{'_end_tag'}; # changes over time
+
+    #local @{ $self }{@CONFIG_COMPILETIME} = @{ $self }{@CONFIG_COMPILETIME};
+
+    my @tree;             # the parsed tree
+    my $pointer = \@tree; # pointer to current tree to handle nested blocks
+    my @state;            # maintain block levels
+    local $self->{'_state'} = \@state; # allow for items to introspect (usually BLOCKS)
+    local $self->{'_in_perl'};         # no interpolation in perl
+    my @in_view;          # let us know if we are in a view
+    my @move_to_front;    # items that need to be declared first (usually BLOCKS)
+    my @meta;             # place to store any found meta information (to go into META)
+    my $post_chomp = 0;   # previous post_chomp setting
+    my $continue   = 0;   # flag for multiple directives in the same tag
+    my $post_op    = 0;   # found a post-operative DIRECTIVE
+    my $capture;          # flag to start capture
+    my $func;
+    my $node;
+    local pos $$str_ref = 0;
+    my $allow_expr = ! defined($self->{'EXPR'}) || $self->{'EXPR'}; # default is on
+
+    while (1) {
+        ### find the next opening tag
+        $$str_ref =~ m{ \G (.*?) $START }gcxs
+            || last;
+
+        my ($text, $comment, $is_close, $func) = ($1, $2, $3, uc $4);
+
+        ### found a text portion - chomp it, interpolate it and store it
+        if (length $text) {
+            my $_last = pos $$str_ref;
+            if ($post_chomp) {
+                if    ($post_chomp == 1) { $_last += length($1)     if $text =~ s{ ^ ([^\S\n]* \n) }{}x  }
+                elsif ($post_chomp == 2) { $_last += length($1) + 1 if $text =~ s{ ^ (\s+)         }{ }x }
+                elsif ($post_chomp == 3) { $_last += length($1)     if $text =~ s{ ^ (\s+)         }{}x  }
+            }
+            if (length $text) {
+                push @$pointer, $text;
+                $self->interpolate_node($pointer, $_last) if $self->{'INTERPOLATE'};
+            }
+        }
+
+        ### make sure we know this directive
+        $func = 'GET' if $func eq 'VAR';
+        if (! $CGI::Ex::Template::DIRECTIVES->{$func}) {
+            $self->throw('parse', "Found unknow DIRECTIVE ($func)", undef, pos($$str_ref) - length($func));
+        }
+        $node = [$func, pos($$str_ref), undef];
+        push @$pointer, $node;
+
+        ### take care of chomping - yes HT now get CHOMP SUPPORT
+        my $pre_chomp = $$str_ref =~ m{ \G ([+=~-]) }gcx ? $1 : $self->{'PRE_CHOMP'};
+        $pre_chomp  =~ y/-=~+/1230/ if $pre_chomp;
+        if ($pre_chomp && $pointer->[-1] && ! ref $pointer->[-1]) {
+            if    ($pre_chomp == 1) { $pointer->[-1] =~ s{ (?:\n|^) [^\S\n]* \z }{}x  }
+            elsif ($pre_chomp == 2) { $pointer->[-1] =~ s{             (\s+) \z }{ }x }
+            elsif ($pre_chomp == 3) { $pointer->[-1] =~ s{             (\s+) \z }{}x  }
+            splice(@$pointer, -1, 1, ()) if ! length $pointer->[-1]; # remove the node if it is zero length
+        }
+
+        ### handle ending tags - or continuation blocks
+        if ($is_close || $CGI::Ex::Template::DIRECTIVES->{$func}->[4]) {
+
+            if (! @state) {
+                $self->throw('parse', "Found an $func tag while not in a block", $node, pos($$str_ref));
+            }
+            my $parent_node = pop @state;
+
+            ### handle continuation blocks such as elsif, else, catch etc
+            if ($CGI::Ex::Template::DIRECTIVES->{$func}->[4]) {
+                pop @$pointer; # we will store the node in the parent instead
+                $parent_node->[5] = $node;
+                my $parent_type = $parent_node->[0];
+                if (! $CGI::Ex::Template::DIRECTIVES->{$func}->[4]->{$parent_type}) {
+                    $self->throw('parse', "Found unmatched nested block", $node, pos($$str_ref));
+                }
+            }
+
+            ### restore the pointer up one level (because we hit the end of a block)
+            $pointer = (! @state) ? \@tree : $state[-1]->[4];
+
+            ### normal end block
+            if (! $CGI::Ex::Template::DIRECTIVES->{$func}->[4]) {
+                if ($CGI::Ex::Template::DIRECTIVES->{$parent_node->[0]}->[5]) { # move things like BLOCKS to front
+                    if ($parent_node->[0] eq 'BLOCK'
+                        && defined($parent_node->[3])
+                        && @in_view) {
+                        push @{ $in_view[-1] }, $parent_node;
+                    } else {
+                        push @move_to_front, $parent_node;
+                    }
+                    if ($pointer->[-1] && ! $pointer->[-1]->[6]) { # capturing doesn't remove the var
+                        splice(@$pointer, -1, 1, ());
+                    }
+                } elsif ($parent_node->[0] =~ /PERL$/) {
+                    delete $self->{'_in_perl'};
+                } elsif ($parent_node->[0] eq 'VIEW') {
+                    my $ref = { map {($_->[3] => $_->[4])} @{ pop @in_view }};
+                    unshift @{ $parent_node->[3] }, $ref;
+                }
+
+
+            ### continuation block - such as an elsif
+            } else {
+                push @state, $node;
+                $pointer = $node->[4] ||= [];
+            }
+
+        } else {
+
+            ### allow for variable escaping (we'll add on a vmethod)
+            my $escape = ($$str_ref =~ m{
+                \G [Ee][Ss][Cc][Aa][Pp][Ee] \s*=\s* ([\"\']?)
+                ([Nn][Oo][Nn][Ee] | [Hh][Tt][Mm][Ll] | [Uu][Rr][Ll] | [Jj][Ss] | [01])
+                \1 \s* }gcx) ? lc($2) : '';
+
+            my $is_expr;
+            my $quote = '';
+            if ($$str_ref =~ m{ \G [Ee][Xx][Pp][Rr] \s*=\s* ([\"\']?) \s* }gcx) {
+                $is_expr = 1;
+                $quote = $1;
+            } elsif ($$str_ref =~ m{ \G [Nn][Aa][Mm][Ee] \s*=\s* ([\"\']?) \s* }gcx) {
+                $quote = $1;
+            }
+
+            ### store what we'll find at the end of the tag
+            $self->{'_end_tag'} = $comment ? qr{$quote\s*([+=~-]?)-->} : qr{$quote\s*([+=~-]?)>};
+
+            #
+            if ($is_expr) {
+                die;
+            } else {
+                $$str_ref =~ m{ \G ([\w./+_]*) }gcx
+                    || $self->throw('parse', 'Error while looking for NAME', undef, pos($$str_ref));
+                $node->[3] = [$1, 0]; # set the variable
+            }
+
+            if ($escape eq 'html' || $escape eq '1') {
+                push @{ $node->[3] }, '|', 'html', 0;
+            } elsif ($escape eq 'url') {
+                push @{ $node->[3] }, '|', 'url', 0;
+            }
+
+
+            ### handle block directives
+            if ($CGI::Ex::Template::DIRECTIVES->{$func}->[2]) {
+                push @state, $node;
+                $pointer = $node->[4] ||= []; # allow future parsed nodes before END tag to end up in current node
+                push @in_view, [] if $func eq 'VIEW';
+            } elsif ($func eq 'META') {
+                unshift @meta, %{ $node->[3] }; # first defined win
+                $node->[3] = undef;             # only let these be defined once - at the front of the tree
+            }
+        }
+
+
+#        ### look for DIRECTIVES
+#        if ($$str_ref =~ m{ \G $QR_DIRECTIVE }gcxo   # find a word
+#            && ($func = $self->{'ANYCASE'} ? uc($1) : $1)
+#            && ($DIRECTIVES->{$func}
+#                || ((pos($$str_ref) -= length $1) && 0))
+#            ) {                       # is it a directive
+#            $$str_ref =~ m{ \G \s* $QR_COMMENTS }gcx;
+#
+#            $node->[0] = $func;
+#
+#            ### store out this current node level to the appropriate tree location
+#            # on a post operator - replace the original node with the new one - store the old in the new
+#            if ($DIRECTIVES->{$func}->[3] && $post_op) {
+#                my @post_op = @$post_op;
+#                @$post_op = @$node;
+#                $node = $post_op;
+#                $node->[4] = [\@post_op];
+#            # handle directive captures for an item like "SET foo = BLOCK"
+#            } elsif ($capture) {
+#                push @{ $capture->[4] }, $node;
+#                undef $capture;
+#                # normal nodes
+#            } else{
+#                push @$pointer, $node;
+#            }
+#
+#            ### parse any remaining tag details
+#            $node->[3] = eval { $DIRECTIVES->{$func}->[0]->($self, $str_ref, $node) };
+#            if (my $err = $@) {
+#                $err->node($node) if UNIVERSAL::can($err, 'node') && ! $err->node;
+#                die $err;
+#            }
+#            $node->[2] = pos $$str_ref;
+#
+#            ### anything that behaves as a block ending
+#            if ($func eq 'END' || $DIRECTIVES->{$func}->[4]) { # [4] means it is a continuation block (ELSE, CATCH, etc)
+
+
+#
+#        ### allow for bare variable getting and setting
+#        } elsif (defined(my $var = $self->parse_expr($str_ref))) {
+#            push @$pointer, $node;
+#            if ($$str_ref =~ m{ \G ($QR_OP_ASSIGN) >? (?! [+=~-]? $END) \s* $QR_COMMENTS }gcx) {
+#                $node->[0] = 'SET';
+#                $node->[3] = eval { $DIRECTIVES->{'SET'}->[0]->($self, $str_ref, $node, $1, $var) };
+#                if (my $err = $@) {
+#                    $err->node($node) if UNIVERSAL::can($err, 'node') && ! $err->node;
+#                    die $err;
+#                }
+#            } else {
+#                $node->[0] = 'GET';
+#                $node->[3] = $var;
+#            }
+#            $node->[2] = pos $$str_ref;
+#        }
+
+        ### look for the closing tag
+        if ($$str_ref =~ m{ \G $self->{'_end_tag'} }gcxs) {
+            $post_chomp = $1 || $self->{'POST_CHOMP'};
+            $post_chomp =~ y/-=~+/1230/ if $post_chomp;
+            $continue = 0;
+            $post_op  = 0;
+            next;
+
+        ### no closing tag
+        } else {
+            $self->throw('parse', "Not sure how to handle tag", $node, pos($$str_ref));
+        }
+    }
+
+    ### cleanup the tree
+    if (@move_to_front) {
+        unshift @tree, @move_to_front;
+    }
+    if (@meta) {
+        unshift @tree, ['META', 0, 0, {@meta}];
+    }
+    if ($#state > -1) {
+        $self->throw('parse', "Missing END tag", $state[-1], 0);
+    }
+
+    ### pull off the last text portion - if any
+    if (pos($$str_ref) != length($$str_ref)) {
+        my $text  = substr $$str_ref, pos($$str_ref);
+        my $_last = pos($$str_ref);
+        if ($post_chomp) {
+            if    ($post_chomp == 1) { $_last += length($1)     if $text =~ s{ ^ ([^\S\n]* \n) }{}x  }
+            elsif ($post_chomp == 2) { $_last += length($1) + 1 if $text =~ s{ ^ (\s+)         }{ }x }
+            elsif ($post_chomp == 3) { $_last += length($1)     if $text =~ s{ ^ (\s+)         }{}x  }
+        }
+        if (length $text) {
+            push @$pointer, $text;
+            $self->interpolate_node($pointer, $_last) if $self->{'INTERPOLATE'};
+        }
+    }
+
+    return \@tree;
+}
+
+###----------------------------------------------------------------###
+
 package CGI::Ex::Template::Context;
 
 use vars qw($AUTOLOAD);
