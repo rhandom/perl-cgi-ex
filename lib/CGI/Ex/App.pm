e6d28a536693e68331d62f99f6b097d69ea5143b
paul <paul>
2007-06-26 20:26:44 +0000

    up_version_to_2.17

--- lib/CGI/Ex/App.pm
+++ lib/CGI/Ex/App.pm
@@ -10,7 +10,7 @@ use strict;
 use vars qw($VERSION);
 
 BEGIN {
-    $VERSION = '2.16';
+    $VERSION = '2.17';
 
     Time::HiRes->import('time') if eval {require Time::HiRes};
     eval {require Scalar::Util};
