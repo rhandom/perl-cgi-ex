e288acefef7b0774742cda576783e3d1a8ec75bd
Paul <paul@bluehost.com>
2012-02-03 12:22:11 -0700

    up version

--- lib/CGI/Ex.pm
+++ lib/CGI/Ex.pm
@@ -24,7 +24,7 @@ use vars qw($VERSION
 use base qw(Exporter);
 
 BEGIN {
-    $VERSION               = '2.35';
+    $VERSION               = '2.36';
     $PREFERRED_CGI_MODULE  ||= 'CGI';
     @EXPORT = ();
     @EXPORT_OK = qw(get_form
