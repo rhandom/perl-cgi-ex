0b17a733b06957adfa0f95051247677a45fa5a9c
Paul <paul@bluehost.com>
2013-09-25 22:16:39 -0600

    update docs

--- lib/CGI/Ex/Template.pm
+++ lib/CGI/Ex/Template.pm
@@ -8,7 +8,7 @@ CGI::Ex::Template - Template::Alloy based TT2/TT3/HT/HTE/Tmpl/Velocity engine.
 
 use strict;
 use warnings;
-use Template::Alloy 1.016;
+use Template::Alloy 1.020;
 use base qw(Template::Alloy);
 use vars qw($QR_PRIVATE
             $WHILE_MAX
@@ -42,115 +42,25 @@ use vars qw($QR_PRIVATE
 
 __END__
 
-=head1 SYNOPSIS
-
-=head2 Template::Toolkit style usage
-
-    my $t = Template::Alloy->new(
-        INCLUDE_PATH => ['/path/to/templates'],
-    );
-
-    my $swap = {
-        key1 => 'val1',
-        key2 => 'val2',
-        code => sub { 42 },
-        hash => {a => 'b'},
-    };
-
-    # print to STDOUT
-    $t->process('my/template.tt', $swap)
-        || die $t->error;
-
-    # process into a variable
-    my $out = '';
-    $t->process('my/template.tt', $swap, \$out);
-
-    ### Alloy uses the same syntax and configuration as Template::Toolkit
-
-
-=head2 HTML::Template::Expr style usage
-
-    my $t = Template::Alloy->new(
-        filename => 'my/template.ht',
-        path     => ['/path/to/templates'],
-    );
-
-    my $swap = {
-        key1 => 'val1',
-        key2 => 'val2',
-        code => sub { 42 },
-        hash => {a => 'b'},
-    };
-
-    $t->param($swap);
-
-    # print to STDOUT (errors die)
-    $t->output(print_to => \*STDOUT);
-
-    # process into a variable
-    my $out = $t->output;
-
-    ### Alloy can also use the same syntax and configuration as HTML::Template
-
-=head2 Text::Tmpl style usage
-
-    my $t = Template::Alloy->new;
-
-    my $swap = {
-        key1 => 'val1',
-        key2 => 'val2',
-        code => sub { 42 },
-        hash => {a => 'b'},
-    };
-
-    $t->set_delimiters('#[', ']#');
-    $t->set_strip(0);
-    $t->set_values($swap);
-    $t->set_dir('/path/to/templates');
-
-    my $out = $t->parse_file('my/template.tmpl');
-
-    my $str = "Foo #[echo $key1]# Bar";
-    my $out = $t->parse_string($str);
-
-
-    ### Alloy uses the same syntax and configuration as Text::Tmpl
-
-=head2 Velocity (VTL) style usage
-
-    my $t = Template::Alloy->new;
-
-    my $swap = {
-        key1 => 'val1',
-        key2 => 'val2',
-        code => sub { 42 },
-        hash => {a => 'b'},
-    };
-
-    my $out = $t->merge('my/template.vtl', $swap);
-
-    my $str = "#set($foo 1 + 3) ($foo) ($bar) ($!baz)";
-    my $out = $t->merge(\$str, $swap);
-
 =head1 DESCRIPTION
+
+Deprecated - use Template::Alloy directly.
 
 CGI::Ex::Template is the original base for the code that is now
 Template::Alloy.  Template::Alloy employed enough complexity and
 featureset to warrant moving it out to a separate namespace.
 
-CGI::Ex::Template is now a place holder subclass of Template::Alloy.
 You can use CGI::Ex::Template as a standalone module - but it is
 suggested that you use Template::Alloy directly instead.
 
-For examples of usage, configuration, syntax, bugs, vmethods,
-directives, etc please refer to the L<Template::Alloy> documentation.
+See the L<Template::Alloy> documentation.
 
 =head1 LICENSE
 
-This module may be distributed under the same terms as Perl itself.
+This module is distributed under the Artistic License.
 
 =head1 AUTHOR
 
-Paul Seamons <perl at seamons dot com>
+Paul Seamons <paul@seamons.com>
 
 =cut
