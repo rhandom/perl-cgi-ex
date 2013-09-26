2c208695dbaadc03163ff0af123ea58b4194f1de
Paul <paul@bluehost.com>
2013-09-25 23:50:34 -0600

    first

---
+++ lib/Cea.pm
@@ -0,0 +1,27 @@
+package Cea;
+
+use base qw(CGI::Ex::App);
+our $VERSION = '2.39';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Cea - Shortcut for accessing CGI::Ex::App
+
+=head1 SYNOPSIS
+
+    use base qw(Cea);
+
+=head1 DESCRIPTION
+
+Cea is a barebones subclass of CGI::Ex::App useful as a namespace,
+and also as a lazy way for accessing CGI::Ex::App.
+
+Pronounced See-ah.
+
+
+
+=cut
